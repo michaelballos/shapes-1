e07e1610584a1b9056a051651e063823ed24b95f
michaelballos <mdb3091207@gmail.com>
2022-03-12 21:28:13 -0600

    Add Functions
    Add degree to radian conversion and sum of internal angles formula. Find vertex position.

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
-const createShape = function vertices(numVertices){
-    const shape = {
-        name: 'Shape',
+const shapeCreator = function shapeCreator(numVertices){  // vertices = 4
+    const shapeProperties = {
+        name: 'buttPlug',
         numVertices: numVertices,
-        findPerimeter (sideLength){
-          return 'Perimeter Calculation';     
+        angleSize: 30,
+        findPerimeter: (sideLength) => {  //sidelength = 4 
+          perimeter = sideLength * numVertices; 
+          return perimeter;    
         },
-        createConvexVertices (sideLength){
-          const vertex = {
-            x: Number,
-            y: Number,
-          }; 
-          return vertex;
+        createConvexVertices: (sideLength) => { 
+          const angles = [60, 60, 60];
+          const distanceFromOrigin = sideLength / (2 * Math.sin(30));
+          const vertices = angles.map(angle => {
+            xVertexPosition = sideLength / 2;
+            yVertexPoisition = math.sqrt(distanceFromOrigin - (xVertexPosition * xVertexPosition));
+          });
+          return (vertices);
         }
-    };    
-    return (shape);
-};
+      };
+    };
+
+degToRad = (degree) => (Math.PI * degree) / 180;
+intAngleSum = (numVertices) => numVertices - 2 * 180;
+
