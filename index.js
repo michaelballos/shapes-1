b92420f29a3b061ba11775901d85a5e053d7e1ef
michaelballos <mdb3091207@gmail.com>
2022-03-14 13:14:25 -0500

    Add Final CreateShape Function
    Create function with input of sideLength and number of vertices. Function calculates inner angles, theta, and theta position by using trig to calculate the distance from origin. Function converts degrees to radian for easier position calculation.

--- index.js
+++ index.js
@@ -1,24 +1,54 @@
-const shapeCreator = function shapeCreator(numVertices){  // vertices = 4
-    const shapeProperties = {
-        name: 'buttPlug',
-        numVertices: numVertices,
-        angleSize: 30,
-        findPerimeter: (sideLength) => {  //sidelength = 4 
-          perimeter = sideLength * numVertices; 
-          return perimeter;    
-        },
-        createConvexVertices: (sideLength) => { 
-          const angles = [60, 60, 60];
-          const distanceFromOrigin = sideLength / (2 * Math.sin(30));
-          const vertices = angles.map(angle => {
-            xVertexPosition = sideLength / 2;
-            yVertexPoisition = math.sqrt(distanceFromOrigin - (xVertexPosition * xVertexPosition));
-          });
-          return (vertices);
-        }
-      };
-    };
+const degToRad = (degree) => (Math.PI * degree) / 180;
+const intAngleSum = (numVertices) => (numVertices - 2) * 180;
+const round = (unrounded) => Math.ceil(unrounded * 10000) / 10000;
 
-degToRad = (degree) => (Math.PI * degree) / 180;
-intAngleSum = (numVertices) => numVertices - 2 * 180;
+const createShape = (numVertices) => {
+  const angleSize = (numVertices - 2) * 180;
+  const innerAngleSize = angleSize / (2 * numVertices)
 
+  const shapeProperties = {
+    name: "buttPlug",
+    numVertices: numVertices,
+    totalAngleSize: angleSize,
+    innerAngleSize: innerAngleSize,
+    createConvexVertices: (sideLength) => {
+      let thetas = [];
+      for(n=0; n<numVertices; n+=1) {
+        const pushTheta = (360/numVertices) * n + 90;
+        thetas.push(pushTheta);
+      }
+      const distanceFromOrigin = sideLength / (2 * Math.cos(degToRad(innerAngleSize)));
+      console.log('distanceFromOrigin', distanceFromOrigin);
+      console.log('thetas', thetas);
+      const vertices = thetas.map(thetas => ({ 
+        x: distanceFromOrigin * Math.cos(degToRad(thetas)),
+        y: distanceFromOrigin * Math.sin(degToRad(thetas)),
+      }));
+      return vertices;
+    },
+    findPerimeter: (sideLength) => {
+      perimeter = sideLength * numVertices;
+      return perimeter;
+    },
+  };
+  return(shapeProperties);
+};
+
+
+const testTriangle = () => {
+  const triangleInterface = createShape(10);
+  console.log('createShapeResult', triangleInterface);
+
+  const trianglePerimeter = triangleInterface.findPerimeter(2);
+  console.log(`Triangle perimeter: ${trianglePerimeter}`);
+
+  const triangleVertices = triangleInterface.createConvexVertices(3);
+
+  triangleVertices.forEach((vertex) => {
+  console.log(`(${round(vertex.x)}, ${round(vertex.y)})`);
+  });
+
+};
+
+
+testTriangle();
